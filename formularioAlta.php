caad4f5a8ef778e0fbe3961d3170fd1723710f8c
LUIS EDGAR <LUIS EDGAR@YAMITSO.lan>
2014-08-21 15:15:37 -0500

    nuevas hojas agregadas

---
+++ formularioAlta.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+</head>
+<body>
+    <form action="" method="POST">
+        <table>
+             <tr><td>Expediente : </td><td><input type="text" name="nombre" required/></td></tr>
+             <tr><td>Nombre : </td><td><input type="text" name="nombre" required/></td></tr>
+             <tr><td>Puesto : </td><td><input type="text" name="nombre" required/></td></tr>
+             <tr><td>Adscripción : </td><td><input type="text" name="nombre" required/></td></tr>
+             
+            
+            
+            
+            
+            
+            
+        </table>
+        
+        
+        
+        
+        
+    </form>
+    
+    
+    
+</body>
+</html>
