27bd88901c9485d11a5fb523e8328785f388fae4
LUIS EDGAR <LUIS EDGAR@YAMITSO.lan>
2014-08-25 15:22:51 -0500

    Formulario de alta terminado, almenos el formato... saludos.

--- instructoresDetallada.php
+++ instructoresDetallada.php
@@ -29,7 +29,7 @@
                 <th colspan="<?php echo $numeroColumnas?>">GERENCIA DE INGENIERIA</th>
             </tr>
             <?php
-                imprimirCabeceraInstruccctores();
+                imprimirCabeceraInstructores();
                 
 
                    
