a9780cddd9488459e2d02b9afbd0c8f1cae59d65
LUIS EDGAR <LUIS EDGAR@YAMITSO.lan>
2014-09-02 14:23:59 -0500

    ola k ase!! programando o k ase!

--- PruebaFormulario.php
+++ PruebaFormulario.php
@@ -8,6 +8,12 @@
         <link href="css/movil.css" rel="stylesheet" type="text/css">
         <link href="css/prueba.css" rel="stylesheet" type="text/css">       
         
+        
+        
+        
+        
+        
+        
 </head>
 <body>
     <div id="content">
@@ -18,25 +24,25 @@
                     <li><a href="ayuda.php" title="Ayuda"><img src="img/baayuda.png"></a></li>
                     <li><a href="close.php" title="Salir"><img src="img/baasalir.png"></a></li>
                 </ul>
-     <form action="" method="post" class="smart-green">
+     <form action="pruebaFormulario_1.php" method="post" class="smart-green">
     <h1>CAPTURA DE INSTRUCTORES
         <span>INGRESA LOS SIGUIENTES DATOS PARA AGREGAR UN INSTRUCTOR</span>
     </h1>
     <label>
         <span>Expediente:</span>
-        <input type="text" name="expediente" required/>
+        <input type="text" name="expediente" />
     </label>
     <label>
         <span>Nombre:</span>
-        <input type="text" name="nombre" required/>
+        <input type="text" name="nombre" />
     </label>
    
     <label>
         <span>Puesto:</span>
-        <input type="text" name="puesto" required/>
+        <input type="text" name="puesto" />
     </label>
      <label>
-        <span>Adscripcion:</span><select name='Adscripción' required>
+        <span>Adscripcion:</span><select name='Adscripción' >
         <option></option>
                 <option value='COORDINACIÓN DE MANTENIMIENTO MAYOR TICOMAN'>COORDINACIÓN DE MANTENIMIENTO MAYOR TICOMAN</option>
                 <option value='COORDINACIÓN DE MANTENIMIENTO MAYOR ZARAGOZA'>COORDINACIÓN DE MANTENIMIENTO MAYOR ZARAGOZA</option>
@@ -49,7 +55,7 @@
                 <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ZARAGOZA'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ZARAGOZA</option>
                 <option value='COORDINACIÓN DE PROGRAMACIÓN Y EVALUACIÓN'>COORDINACIÓN DE PROGRAMACIÓN Y EVALUACIÓN</option>
                 <option value='COORDINACIÓN DE REHABILITACIÓN DE TRENES'>COORDINACIÓN DE REHABILITACIÓN DE TRENES</option>
-                <option value='COORDINACIÓN DE REHABILITACIÓN DE TRENES'>COORDINACIÓN DE REHABILITACIÓN DE TRENES</option>
+                
                 <option value='COORDINACIÓN ELECTRÓNICA'>COORDINACIÓN ELECTRÓNICA</option>
                 <option value='DIRECCIÓN DE MANTENIMIENTO DE MATERIAL RODANTE'>DIRECCIÓN DE MANTENIMIENTO DE MATERIAL RODANTE</option>
                 <option value='GERENCIA DE INGENIERÍA'>GERENCIA DE INGENIERÍA</option>
@@ -61,33 +67,83 @@
         </select>
     </label>  
    <label>
+        <span>Coordinacion:</span><select name='coordinacion' >
+        <option></option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO MAYOR TICOMAN'>COORDINACIÓN DE MANTENIMIENTO MAYOR TICOMAN</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO MAYOR ZARAGOZA'>COORDINACIÓN DE MANTENIMIENTO MAYOR ZARAGOZA</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CD. AZTECA'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CD. AZTECA</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CONSTTUCIÓN DE 1917'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CONSTTUCIÓN DE 1917</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO LA PAZ'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO LA PAZ</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ROSARIO'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ROSARIO</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TAXQUEÑA'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TAXQUEÑA</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TICOMAN'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TICOMAN</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ZARAGOZA'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ZARAGOZA</option>
+                <option value='COORDINACIÓN DE PROGRAMACIÓN Y EVALUACIÓN'>COORDINACIÓN DE PROGRAMACIÓN Y EVALUACIÓN</option>
+                <option value='COORDINACIÓN DE REHABILITACIÓN DE TRENES'>COORDINACIÓN DE REHABILITACIÓN DE TRENES</option>
+                
+                <option value='COORDINACIÓN ELECTRÓNICA'>COORDINACIÓN ELECTRÓNICA</option>
+                <option value='DIRECCIÓN DE MANTENIMIENTO DE MATERIAL RODANTE'>DIRECCIÓN DE MANTENIMIENTO DE MATERIAL RODANTE</option>
+                <option value='GERENCIA DE INGENIERÍA'>GERENCIA DE INGENIERÍA</option>
+                <option value='GERENCIA DE INSTALACIONES FIJAS'>GERENCIA DE INSTALACIONES FIJAS</option>
+                <option value='SUBGERENCIA DE MANTENIMIENTO MAYOR Y REHABILITACIÓN'>SUBGERENCIA DE MANTENIMIENTO MAYOR Y REHABILITACIÓN</option>
+                <option value='SUBGERENCIA DE MANTENIMIENTO SISTEMÁTICO I'>SUBGERENCIA DE MANTENIMIENTO SISTEMÁTICO I</option>
+                <option value='SUBGERENCIA DE MANTENIMIENTO SISTEMÁTICO II'>SUBGERENCIA DE MANTENIMIENTO SISTEMÁTICO II</option>
+             
+        </select>
+    </label>  
+   <label>
+        <span>Ubicación:</span><select name='ubicacion' >
+        <option></option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO MAYOR TICOMAN'>COORDINACIÓN DE MANTENIMIENTO MAYOR TICOMAN</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO MAYOR ZARAGOZA'>COORDINACIÓN DE MANTENIMIENTO MAYOR ZARAGOZA</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CD. AZTECA'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CD. AZTECA</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CONSTTUCIÓN DE 1917'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CONSTTUCIÓN DE 1917</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO LA PAZ'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO LA PAZ</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ROSARIO'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ROSARIO</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TAXQUEÑA'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TAXQUEÑA</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TICOMAN'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TICOMAN</option>
+                <option value='COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ZARAGOZA'>COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ZARAGOZA</option>
+                <option value='COORDINACIÓN DE REHABILITACIÓN DE TRENES'>COORDINACIÓN DE REHABILITACIÓN DE TRENES</option>
+                <option value='COORDINACIÓN ELECTRÓNICA'>COORDINACIÓN ELECTRÓNICA</option>
+                <option value='DIRECCIÓN DE MANTENIMIENTO DE MATERIAL RODANTE'>DIRECCIÓN DE MANTENIMIENTO DE MATERIAL RODANTE</option>
+                <option value='GERENCIA DE INGENIERÍA'>GERENCIA DE INGENIERÍA</option>
+                <option value='GERENCIA DE INSTALACIONES FIJAS'>GERENCIA DE INSTALACIONES FIJAS</option>
+                <option value='SUBGERENCIA DE MANTENIMIENTO MAYOR Y REHABILITACIÓN'>SUBGERENCIA DE MANTENIMIENTO MAYOR Y REHABILITACIÓN</option>
+                <option value='SUBGERENCIA DE MANTENIMIENTO SISTEMÁTICO I'>SUBGERENCIA DE MANTENIMIENTO SISTEMÁTICO I</option>
+                <option value='SUBGERENCIA DE MANTENIMIENTO SISTEMÁTICO II'>SUBGERENCIA DE MANTENIMIENTO SISTEMÁTICO II</option>
+                <option value='ADMINISTRACIÓN DE MATERIALES'>ADMINISTRACIÓN DE MATERIALES</option>
+                <option value='COORDINACIÓN DE PROGRAMACIÓN Y EVALUACIÓN (JEFATURA TICOMÁN)'>COORDINACIÓN DE PROGRAMACIÓN Y EVALUACIÓN (JEFATURA TICOMÁN)</option>
+                <option value='COMISIÓN SINDICAL'>COMISIÓN SINDICAL</option>
+        </select>
+    </label>
+   <label>
         <span>Plaza:</span>
-        <input type="text" name="plaza" required/>
+        <input type="text" name="plaza" />
     </label>
    <label>
         <span>Siden:</span>
-        <input type="text" name="siden" required/>
+        <input type="text" name="siden" />
     </label>
     <label> <span>Calidad:</span></label>
-            <input type="radio" name='calidad' value='base' required/> BASE
-            <input type="radio" name='calidad' value='confianza' required/> CONFIANZA
-            <input type="radio" name='calidad' value='estructura' required/> ESTRUCTURA
-            <input type="radio" name='calidad' value='honorarios' required/> HONORARIOS
+            <input type="radio" name='calidad' value='base' /> BASE
+            <input type="radio" name='calidad' value='confianza' /> CONFIANZA
+            <input type="radio" name='calidad' value='estructura'/> ESTRUCTURA
+            <input type="radio" name='calidad' value='honorarios' /> HONORARIOS
             
     <label>
         <span>Área:</span>
-        <input type="text" name="area" required/>
+        <input type="text" name="area" />
     </label>
     <label>
         <span>Horario:</span>
-        <input type="text" name="horario" placeholder='EJEMPLO:07:00 A 15:00' required/>
+        <input type="text" name="horario" placeholder='EJEMPLO:07:00 A 15:00' />
     </label>
     <label>
         <span>Descanso:</span>
-        <input type="text" name="descanso" placeholder='EJEMPLO: S Y D, L Y M.' required/>
+        <input type="text" name="descanso" placeholder='EJEMPLO: S Y D, L Y M.' />
     </label>
     <label>
-        <span>Ultimo grado de estudios:</span><select name='estudios' required>
+        <span>Ultimo grado de estudios:</span><select name='estudios' >
             <option></option>
                        <option value='Primaria'>Primaria</option>
                        <option value='Secundaria'>Secundaria</option>
@@ -101,36 +157,36 @@
         </select>
     </label>
     <label> <span>¿Concluido?:</span></label>
-           <input type="radio" name='concluido' value='si' required/> Si
-           <input type="radio" name='concluido' value='no' required/> No
+           <input type="radio" name='concluido' value='si' /> Si
+           <input type="radio" name='concluido' value='no' /> No
     
     <label>
         <span>Extension:</span>
-        <input type="text" name="extension" required/>
+        <input type="text" name="extension" />
     </label>        
    <label>
         <span>Correo:</span>
-        <input type="text" name="correo"  required/>
+        <input type="text" name="correo"  />
     </label> 
            
     <label> <span>Instructor:</span></label>
-           <input type="radio" name='instructor' value='si' required/> Si
-            <input type="radio" name='instructor' value='no' required/> No
+           <input type="radio" name='instructor' value='si' /> Si
+            <input type="radio" name='instructor' value='no' /> No
             <br>   
     <label>
         <span>¿Tomo el curso de formación de instructores?</span></label>
-        <input type="radio" name='formacion' value='si' required/> Si
-        <input type="radio" name='formacion' value='no' required/> No
+        <input type="radio" name='formacion' value='si' /> Si
+        <input type="radio" name='formacion' value='no' /> No
     
         <br>   
         <br>
         
      <label>
         
-        <input type="button" class="button" value="enviar datos" />
+        <input type="submit"  class="button" value="enviar datos" />
     </label>    
 </form>
-            </div>
+     </div>
     </div>
 </body>
 </html>
