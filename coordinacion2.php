078ced84af9761774191cd9b196ac83be2eeca3f
JuanAlfredo <JuanAlfredo@Kayo.lan>
2014-09-02 14:13:15 -0500

    Actualizado el 2 de septiembre de 2014

--- coordinacion2.php
+++ coordinacion2.php
@@ -16,37 +16,35 @@
                 </ul>
             </div>
             <div id="menu">
-                <ul>
-                    
-                    
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE REHABILITACIÓN DE TRENES';?>'><img src="img/CCSFT.png" class="grande"></a></li>
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE ELECTRÓNICA';?>'><img src="img/CCO.png" class="grande"></a></li>
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE PROGRAMACIÓN Y EVALUACIÓN';?>'><img src="img/CPEC.png"class="grande" ></a></li>
+                <ul>                                        
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE REHABILITACIÓN DE TRENES';?>'><img src="img/CCSFT.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE ELECTRÓNICA';?>'><img src="img/CCO.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE PROGRAMACIÓN Y EVALUACIÓN';?>'><img src="img/CPEC.png"class="grande" ></a></li>
                     
                     
                     
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ZARAGOZA';?>'><img src="img/CCMSZ.png" class="grande"></a></li>
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CONSTITUCIÓN 1917';?>'><img src="img/CCMSC.png" class="grande"></a></li>
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TASQUEÑA';?>'><img src="img/CCMST.png" class="grande"></a></li>
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO TLÁHUAC';?>'><img src="img/CCMSTL.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ZARAGOZA';?>'><img src="img/CCMSZ.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CONSTITUCIÓN 1917';?>'><img src="img/CCMSC.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TASQUEÑA';?>'><img src="img/CCMST.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO TLÁHUAC';?>'><img src="img/CCMSTL.png" class="grande"></a></li>
                     
                    
                     
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ROSARIO';?>'><img src="img/CCMSR.png" class="grande"></a></li>
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO LA PAZ';?>'><img src="img/CCMSLP.png" class="grande"></a></li>
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TICOMÁN';?>'><img src="img/CCMSTI.png" class="grande"></a></li>
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CD. AZTECA';?>'><img src="img/CCMSCAZ.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ROSARIO';?>'><img src="img/CCMSR.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO LA PAZ';?>'><img src="img/CCMSLP.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TICOMÁN';?>'><img src="img/CCMSTI.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CD. AZTECA';?>'><img src="img/CCMSCAZ.png" class="grande"></a></li>
                     
                     
                     
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO MAYOR ZARAGOZA';?>'><img src="img/CCMMZ2.png" class="grande"></a></li>
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO MAYOR TICOMÁN';?>'><img src="img/CCCMTIC.png" class="grande"></a></li>
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=COORDINACIÓN DE REHABILITACIÓN DE TRENES';?>'><img src="img/CCRT.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO MAYOR ZARAGOZA';?>'><img src="img/CCMMZ2.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE MANTENIMIENTO MAYOR TICOMÁN';?>'><img src="img/CCCMTIC.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=COORDINACIÓN DE REHABILITACIÓN DE TRENES';?>'><img src="img/CCRT.png" class="grande"></a></li>
                     
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=SUBGERENCIA DE MANTENIMIENTO MAYOR Y REHABILITACIÓN';?>'><img src="img/CSMS1.png" class="grande"></a></li>
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=SUBGERENCIA DE MANTENIMIENTO SISTEMÁTICO II';?>'><img src="img/CSMS2.png" class="grande"></a></li>
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=SUBGERENCIA DE MANTENIMIENTO SISTEMÁTICO I';?>'><img src="img/CSMMR.png" class="grande"></a></li>
-                    <li><a name="coordinacion" href='<?php echo 'personalCoordinacion.php?coordinacion=GERENCIA DE INGENIERÍA';?>'><img src="img/GI.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=SUBGERENCIA DE MANTENIMIENTO MAYOR Y REHABILITACIÓN';?>'><img src="img/CSMS1.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=SUBGERENCIA DE MANTENIMIENTO SISTEMÁTICO II';?>'><img src="img/CSMS2.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=SUBGERENCIA DE MANTENIMIENTO SISTEMÁTICO I';?>'><img src="img/CSMMR.png" class="grande"></a></li>
+                    <li><a name="coordinacion" href='<?php echo 'instructoresCoordinacion.php?coordinacion=GERENCIA DE INGENIERÍA';?>'><img src="img/GI.png" class="grande"></a></li>
                 </ul>
             </div>
         </div>
---
+++ instructoresUbicacion.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title>INSTRUCTORES POR UBICACIÓN</title>
+        <link href="css/movil.css" rel="stylesheet" type="text/css">
+        <link href="css/tabla2.css" rel="stylesheet" type="text/css"> 
+    </head>
+    <body>
+        <div id="content">
+            <div id="nav">
+                <ul>
+                    <li><a href="ubicacion2.php" title="Regresar"><img src="img/baatras.png"></a></li>
+                    <li><a href="menu.php" title="Inicio"><img src="img/bahome.png"></a></li>
+                    <li><a href="ayuda.php" title="Ayuda"><img src="img/baayuda.png"></a></li>
+                    <li><a href="close.php" title="Salir"><img src="img/baasalir.png"></a></li>
+                </ul>
+            </div>
+            <?php                                      
+                if (isset($_GET['ubicacion'])) {
+                    $ubicacion = mysql_real_escape_string($_GET['ubicacion']);
+                } else {
+                    $ubicacion="";
+                    echo "<table cellpadding='2' cellspacing='2' border='2'>
+                        <tr><td>ERROR EN LA PÁGINA</td></tr>
+                    </table>";
+                }         
+                require 'usarBD.php';            
+                require 'claseEmpleado.php';
+                if(strcmp($ubicacion, "COMISIONADOS") == 0) {
+                    $consulta = "SELECT * FROM general WHERE instructor=TRUE AND ubicacion<>\"SUBGERENCIA DE MANTENIMIENTO MAYOR Y REHABILITACIÓN\"
+                        AND ubicacion<>\"COORDINACIÓN DE MANTENIMIENTO MAYOR ZARAGOZA\"
+                        AND ubicacion<>\"COORDINACIÓN DE MANTENIMIENTO MAYOR TICOMÁN\"
+                        AND ubicacion<>\"COORDINACIÓN DE REHABILITACIÓN DE TRENES\"
+                        AND ubicacion<>\"SUBGERENCIA DE MANTENIMIENTO SISTEMÁTICO I\"
+                        AND ubicacion<>\"COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ZARAGOZA\"
+                        AND ubicacion<>\"COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CONSTITUCIÓN 1917\"
+                        AND ubicacion<>\"COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TASQUEÑA\"
+                        AND ubicacion<>\"COORDINACIÓN DE MANTENIMIENTO TLÁHUAC\"
+                        AND ubicacion<>\"SUBGERENCIA DE MANTENIMMIENTO SISTEMÁTICO II\"
+                        AND ubicacion<>\"COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO ROSARIO\"
+                        AND ubicacion<>\"COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO LA PAZ\"
+                        AND ubicacion<>\"COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO TICOMÁN\"
+                        AND ubicacion<>\"COORDINACIÓN DE MANTENIMIENTO SISTEMÁTICO CD. AZTECA\"
+                        AND ubicacion<>\"GERENCIA DE INGENIERIA\"
+                        AND ubicacion<>\"COORDINACIÓN DE SUPERVISIÓN DE FABRICACIÓN DE TRENES\"
+                        AND ubicacion<>\"COORDINACIÓN DE ELECTRÓNICA\"
+                        AND ubicacion<>\"COORDINACIÓN DE PROGRAMACIÓN Y EVALUACIÓN (JEFATURA TICOMÁN)\"
+                        AND ubicacion<>\"ADMINISTRACIÓN DE MATERIALES\";";
+                } else {
+                    $consulta = "SELECT * FROM general WHERE instructor=TRUE AND ubicacion='$ubicacion';";
+                }
+                $hacerConsulta = mysql_query($consulta, $conexion);
+                $numeroDeRegistros = mysql_num_rows($hacerConsulta);
+        
+                if($numeroDeRegistros != 0) {
+                    echo "<table cellpading='2' cellspacing='2' border='2'>
+                            <td>$ubicacion</td>
+                        </table><br><br>";
+                    echo "<table cellpadding='2' cellspacing='2' border='2'>
+                            <tr>
+                                <th colspan='9'>TABLA DETALLADA</th>
+                                <th colspan='2'>Total = $numeroDeRegistros</th>
+                            </tr>";                
+                    imprimirCabecera();
+                    $empleadoActual = new empleado();
+                    for ($i = 0; $i < $numeroDeRegistros; $i++) {
+                        $empleadoActual->asignar($hacerConsulta, $i);
+                        $empleadoActual->imprimirEmpleado();
+                    }
+                    echo "</table>";
+                } else {                    
+                    echo "<table cellpading='2' cellspacing='2' border='2'>
+                            <td>NO HAY REGISTROS DISPONIBLES</td>
+                        </table>";
+                }
+            ?>
+        </div>
+    </body>
+</html>
